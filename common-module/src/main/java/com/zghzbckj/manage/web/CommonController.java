c9acb6d3c80cd97f34870499695cf90c1bcc040b
xuby <109030742@qq.com>
2019-08-29 17:50:47 +0800

    init-网关和注册中心

---
+++ common-module/src/main/java/com/zghzbckj/manage/web/CommonController.java
@@ -0,0 +1,20 @@
+package com.zghzbckj.manage.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <dl>
+ * <dt>CommonController</dt>
+ * <dd>Description:</dd>
+ * <dd>Copyright: Copyright (C) 2019</dd>
+ * <dd>Company:</dd>
+ * <dd>CreateDate: 2019/8/29</dd>
+ * </dl>
+ *
+ * @author xby
+ */
+@RestController
+@RequestMapping(value = "common")
+public class CommonController {
+}
