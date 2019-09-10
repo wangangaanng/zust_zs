d420609b7ff29fb0907484731d52e40136c1e5f3
benyuan <D.chen.g@outlook.com>
2019-09-10 15:05:21 +0800

    栏目修改

--- common-module/src/main/java/com/zghzbckj/manage/entity/BckjBizYhgl.java
+++ common-module/src/main/java/com/zghzbckj/manage/entity/BckjBizYhgl.java
@@ -3,11 +3,11 @@
  */
 package com.zghzbckj.manage.entity;
 
-import org.hibernate.validator.constraints.Length;
-import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import com.zghzbckj.base.entity.DataWithExpEntity;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.Date;
 
 /**
  * ccEntity
@@ -30,9 +30,6 @@ public class BckjBizYhgl extends DataWithExpEntity<BckjBizYhgl> {
 		super();
 	}
 
-	public BckjBizYhgl(String id){
-		super(id);
-	}
 
 
 	
