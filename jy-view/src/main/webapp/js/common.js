d4348a8c9b23fe21292e7ca8200b0571df264a61
zhangqiaojun <897562540@qq.com>
2019-09-11 16:20:23 +0800

    zqj update header

--- jy-view/src/main/webapp/js/common.js
+++ jy-view/src/main/webapp/js/common.js
@@ -8,17 +8,17 @@ $(document).ready(function () {
 })
 var data={nav:[
     {url:'',title:'首页'},
-    {url:'',title:'学院概况'},
-    {url:'',title:'新闻公告'},
-    {url:'',title:'招聘信息'},
-    {url:'',title:'职业指导'},
-    {url:'',title:'企业指南'},
-    {url:'',title:'学生服务'},
+    {url:'',title:'学院概况',sub:[{url:'',title:'学校简介'},{url:'',title:'学院专业'},{url:'',title:'中心介绍'}]},
+    {url:'',title:'新闻公告',sub:[{url:'',title:'通知公告'},{url:'',title:'新闻快递'},{url:'',title:'校内公示'}]},
+    {url:'',title:'招聘信息',sub:[{url:'',title:'浙科院·职来职往'},{url:'',title:'社会招聘会'},{url:'',title:'企业招聘信息'},{url:'',title:'职位招聘信息'},{url:'',title:'招考公告'}]},
+    {url:'',title:'职业指导',sub:[{url:'',title:'政策法规'},{url:'',title:'就业指导'},{url:'',title:'创业指导'},{url:'',title:'生涯规划'},{url:'',title:'技能培训'}]},
+    {url:'',title:'企业指南',sub:[{url:'',title:'招聘指南'},{url:'',title:'生源速览'}]},
+    {url:'',title:'学生服务',sub:[{url:'',title:'办事流程'},{url:'',title:'常用下载'},{url:'',title:'档案查询'}]},
     {url:'',title:'联系我们'},
 ]}
 var headerTpl='<header><div class="top"><div class="container"><div class="top-logo"><img class="logo" src="../img/logo-zust.png"><div class="title">就业信息网</div> </div>'+
     '<div class="nav"><ul>{{>navList}}</ul></div> </div> </div></header>'
-var headerPart={navList:"{{#nav}}<li>{{title}}</li>{{/nav}}"}
+var headerPart={navList:"{{#nav}}<li>{{title}}<ul class='subnav'>{{>subnavList}}</ul></li>{{/nav}}",subnavList:"{{#sub}}<li><a href='#'><span>{{title}}</span></a></li>{{/sub}}"}
 function mustache_init() {
     var header=Mustache.render(headerTpl, data,headerPart);
     $("body").prepend(header);
