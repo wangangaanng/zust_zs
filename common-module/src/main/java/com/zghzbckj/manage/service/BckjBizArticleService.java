55334a6504594bd536b028e6d7130f525d651edc
benyuan <D.chen.g@outlook.com>
2019-09-11 17:23:37 +0800

    就业展示端

--- common-module/src/main/java/com/zghzbckj/manage/service/BckjBizArticleService.java
+++ common-module/src/main/java/com/zghzbckj/manage/service/BckjBizArticleService.java
@@ -3,6 +3,7 @@
  */
 package com.zghzbckj.manage.service;
 
+import com.google.common.collect.Maps;
 import com.ourway.base.utils.BeanUtil;
 import com.ourway.base.utils.JsonUtil;
 import com.ourway.base.utils.TextUtils;
@@ -12,8 +13,10 @@ import com.zghzbckj.base.model.FilterModel;
 import com.zghzbckj.base.model.ResponseMessage;
 import com.zghzbckj.base.service.CrudService;
 import com.zghzbckj.manage.dao.BckjBizArticleDao;
+import com.zghzbckj.manage.dao.CommonDao;
 import com.zghzbckj.manage.entity.BckjBizArticle;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,7 @@ import java.util.Map;
 
 /**
  * ccService
+ *
  * @author cc
  * @version 2019-09-09
  */
@@ -31,91 +35,126 @@ import java.util.Map;
 @Transactional(readOnly = true)
 public class BckjBizArticleService extends CrudService<BckjBizArticleDao, BckjBizArticle> {
 
-	private static final Logger log = Logger.getLogger(BckjBizArticleService.class);
+    private static final Logger log = Logger.getLogger(BckjBizArticleService.class);
+
+    @Autowired
+    CommonDao commonDao;
+    @Override
+    public BckjBizArticle get(String owid) {
+        return super.get(owid);
+    }
 
     @Override
-	public BckjBizArticle get(String owid) {
-		return super.get(owid);
-	}
-	@Override
-	public List<BckjBizArticle> findList(BckjBizArticle bckjBizArticle) {
-		return super.findList(bckjBizArticle);
-	}
-	@Override
-	public PageInfo<BckjBizArticle> findPage(Page<BckjBizArticle> page, BckjBizArticle bckjBizArticle) {
-		return super.findPage(page, bckjBizArticle);
-	}
-	
-	@Transactional(readOnly = false)
-	public void save(BckjBizArticle bckjBizArticle) {
-		super.saveOrUpdate(bckjBizArticle);
-	}
-	@Override
-	@Transactional(readOnly = false)
-	public void delete(BckjBizArticle bckjBizArticle) {
-		super.delete(bckjBizArticle);
-	}
+    public List<BckjBizArticle> findList(BckjBizArticle bckjBizArticle) {
+        return super.findList(bckjBizArticle);
+    }
+
+    @Override
+    public PageInfo<BckjBizArticle> findPage(Page<BckjBizArticle> page, BckjBizArticle bckjBizArticle) {
+        return super.findPage(page, bckjBizArticle);
+    }
+
+    @Transactional(readOnly = false)
+    public void save(BckjBizArticle bckjBizArticle) {
+        super.saveOrUpdate(bckjBizArticle);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void delete(BckjBizArticle bckjBizArticle) {
+        super.delete(bckjBizArticle);
+    }
 
 
-	/**
+    /**
      * <p>方法:findPagebckjBizArticle TODO后台BckjBizArticle分页列表</p>
      * <ul>
-    * <li> @param filters TODO</li>
-    * <li> @param pageNo TODO</li>
-    * <li> @param pageSize TODO</li>
-    * <li>@return com.zghzbckj.base.model.ResponseMessage  </li>
-    * <li>@author D.cehn.g </li>
-    * <li>@date 2018/9/5 9:47  </li>
-    * </ul>
+     * <li> @param filters TODO</li>
+     * <li> @param pageNo TODO</li>
+     * <li> @param pageSize TODO</li>
+     * <li>@return com.zghzbckj.base.model.ResponseMessage  </li>
+     * <li>@author D.cehn.g </li>
+     * <li>@date 2018/9/5 9:47  </li>
+     * </ul>
      */
     public ResponseMessage findPageBckjBizArticle(List<FilterModel> filters, Integer pageNo, Integer pageSize) {
-    Map<String, Object> dataMap = FilterModel.doHandleMap(filters);
-    PageInfo<BckjBizArticle> page = findPage(dataMap, pageNo, pageSize, null);
+        Map<String, Object> dataMap = FilterModel.doHandleMap(filters);
+        PageInfo<BckjBizArticle> page = findPage(dataMap, pageNo, pageSize, null);
         return ResponseMessage.sendOK(page);
-        }
+    }
 
-        /**
-        * <p>方法:savebckjBizArticle TODO保存BckjBizArticle信息 </p>
-        * <ul>
-            * <li> @param mapData TODO</li>
-            * <li>@return com.zghzbckj.base.model.ResponseMessage  </li>
-            * <li>@author D.chen.g </li>
-            * <li>@date 2018/9/6 17:05  </li>
-            * </ul>
-        */
-        @Transactional(readOnly = false)
-        public ResponseMessage saveBckjBizArticle(Map<String, Object> mapData) {
+    /**
+     * <p>方法:savebckjBizArticle TODO保存BckjBizArticle信息 </p>
+     * <ul>
+     * <li> @param mapData TODO</li>
+     * <li>@return com.zghzbckj.base.model.ResponseMessage  </li>
+     * <li>@author D.chen.g </li>
+     * <li>@date 2018/9/6 17:05  </li>
+     * </ul>
+     */
+    @Transactional(readOnly = false)
+    public ResponseMessage saveBckjBizArticle(Map<String, Object> mapData) {
         BckjBizArticle bckjBizArticle = JsonUtil.map2Bean(mapData, BckjBizArticle.class);
-        if(!TextUtils.isEmpty(mapData.get("owid"))){
-        BckjBizArticle bckjBizArticleIndata=get(mapData.get("owid").toString());
-        BeanUtil.copyPropertiesIgnoreNull(bckjBizArticle,bckjBizArticleIndata);
-        bckjBizArticle=bckjBizArticleIndata;
+        if (!TextUtils.isEmpty(mapData.get("owid"))) {
+            BckjBizArticle bckjBizArticleIndata = get(mapData.get("owid").toString());
+            BeanUtil.copyPropertiesIgnoreNull(bckjBizArticle, bckjBizArticleIndata);
+            bckjBizArticle = bckjBizArticleIndata;
         }
         saveOrUpdate(bckjBizArticle);
         return ResponseMessage.sendOK(bckjBizArticle);
-        }
+    }
 
-        /**
-        *<p>方法:removeOrder TODO多条删除BckjBizArticle </p>
-        *<ul>
-            *<li> @param codes TODO</li>
-            *<li>@return com.zghzbckj.base.model.ResponseMessage  </li>
-            *<li>@author D.chen.g </li>
-            *<li>@date 2018/9/6 17:14  </li>
-            *</ul>
-        */
-        @Transactional(readOnly = false)
-        public ResponseMessage removeOrder(List<String> codes) {
-            List<Map<String, Object>> objs = new ArrayList<Map<String, Object>>();
-            for (String owid : codes) {
+    /**
+     * <p>方法:removeOrder TODO多条删除BckjBizArticle </p>
+     * <ul>
+     * <li> @param codes TODO</li>
+     * <li>@return com.zghzbckj.base.model.ResponseMessage  </li>
+     * <li>@author D.chen.g </li>
+     * <li>@date 2018/9/6 17:14  </li>
+     * </ul>
+     */
+    @Transactional(readOnly = false)
+    public ResponseMessage removeOrder(List<String> codes) {
+        List<Map<String, Object>> objs = new ArrayList<Map<String, Object>>();
+        for (String owid : codes) {
             Map<String, Object> params = new HashMap<String, Object>(1);
             BckjBizArticle bckjBizArticle = new BckjBizArticle();
-        bckjBizArticle.setOwid(owid);
+            bckjBizArticle.setOwid(owid);
             this.dao.delete(bckjBizArticle);
             params.put("owid", owid);
             objs.add(params);
-            }
-            return ResponseMessage.sendOK(objs);
-            }
-	
+        }
+        return ResponseMessage.sendOK(objs);
+    }
+
+    public PageInfo getWzList(Map<String, Object> mapData) {
+        String pageNo=mapData.get("pageNo").toString();
+        String pageSize=mapData.get("pageSize").toString();
+        Page<BckjBizArticle> page = new Page(Integer.valueOf(pageNo), Integer.valueOf(pageSize));
+        mapData.put("page", page);
+        mapData.put("orderBy", " a.istop DESC,a.sxh ");
+        page.setList(this.dao.findMapByShort(mapData));
+        PageInfo<BckjBizArticle> pageInfo = new PageInfo();
+        pageInfo.setRecords(page.getList());
+        pageInfo.setTotalPage((long)page.getTotalPage());
+        pageInfo.setCurrentIndex((long)page.getPageNo());
+        pageInfo.setPageSize((long)page.getPageSize());
+        pageInfo.setTotalCount(page.getCount());
+        pageInfo.setCurrentPage((long)page.getPageNo());
+        return pageInfo;
+    }
+
+    public BckjBizArticle getWzDetail(Map<String, Object> mapData) {
+        List<BckjBizArticle> articleList=this.dao.findListByMap(mapData);
+        if(null!=articleList&&articleList.size()>0){
+            BckjBizArticle oneArt=articleList.get(0);
+            Map mapParam= Maps.newHashMap();
+            mapParam.put("wzRefOwid",oneArt.getOwid());
+            List<Map> files=commonDao.getSysFiles(mapParam);
+            oneArt.setFileList(files);
+            return oneArt;
+        }else{
+            return null;
+        }
+    }
 }
