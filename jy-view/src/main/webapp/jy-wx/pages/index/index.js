d38cca44a780d8428311ac73da353ed9933757d5
zhangqiaojun <897562540@qq.com>
2019-09-16 15:05:59 +0800

    add vant

--- jy-view/src/main/webapp/jy-wx/pages/index/index.js
+++ jy-view/src/main/webapp/jy-wx/pages/index/index.js
@@ -5,6 +5,7 @@ const app = getApp()
 Page({
   data: {
     imgUrls: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1679456154,2493198454&fm=26&gp=0.jpg','https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1679456154,2493198454&fm=26&gp=0.jpg'],
+    menuList: [{ text: '学校概况', icon: '' }, { text: '专业介绍', icon: '' }, { text: '新闻公告', icon: '' }, { text: '招聘信息', icon: '' }, { text: '职业指导', icon: '' }, { text: '就业排行榜', icon: '' },{ text: '学生服务', icon: '' }, { text: '企业服务', icon: '' }],
   },
   onLoad: function () {
     
