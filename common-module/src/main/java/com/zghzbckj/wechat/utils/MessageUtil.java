341658efc2940cd769ea3ae1da5cf1dfd4dbc76c
benyuan <D.chen.g@outlook.com>
2019-09-17 10:00:17 +0800

    加wx

--- common-module/src/main/java/com/zghzbckj/wechat/utils/MessageUtil.java
+++ common-module/src/main/java/com/zghzbckj/wechat/utils/MessageUtil.java
@@ -5,10 +5,10 @@ import com.thoughtworks.xstream.core.util.QuickWriter;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
 import com.thoughtworks.xstream.io.xml.XppDriver;
-import com.zghzbckj.messageapi.wechat.model.MusicMessage;
-import com.zghzbckj.messageapi.wechat.model.NewsMessage;
-import com.zghzbckj.messageapi.wechat.model.TextMessage;
-import com.zghzbckj.messageapi.wechat.utils.response.Article;
+import com.zghzbckj.wechat.model.MusicMessage;
+import com.zghzbckj.wechat.model.NewsMessage;
+import com.zghzbckj.wechat.model.TextMessage;
+import com.zghzbckj.wechat.utils.response.Article;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
@@ -21,6 +21,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 /**
  * <p>方法: 消息工具类 </p>
