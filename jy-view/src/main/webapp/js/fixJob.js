77fe89fbd2eea8e8b87bb470cbd2aab80d6ccd3b
moyufeng <741912526@qq.com>
2019-09-24 19:17:00 +0800

    add-mo

--- jy-view/src/main/webapp/js/fixJob.js
+++ jy-view/src/main/webapp/js/fixJob.js
@@ -49,6 +49,10 @@ $(document).ready(function () {
     $("#zwYyyq").val($("#zwYyyq").attr("data-val"))
     $("#zwGwzz").val($("#zwGwzz").attr("data-val"))
 
+    jQuery.validator.addMethod("isNum", function(value, element) {
+        return this.optional(element) || !isNaN(value);
+    }, "请填写数字");
+
     $("#registerForm").validate({
         rules: {
             zwbt:"required",
@@ -57,17 +61,15 @@ $(document).ready(function () {
             zwArea:"required",
             zwGzzn:"required",
             zwGzxz:"required",
-            qyLxrdh:{
-                required: true,
-                isMobile: true
-            },
             zwLxyx:{
                 required: true,
                 email: true
             },
-            zwXs:"required",
+            zwXs:{
+                required: true,
+                isNum: true
+            },
             zwZprs:"required",
-            zwXs:"required",
             zwNlyq:"required",
             zwXlyq:"required",
             zwGznx:"required",
@@ -81,17 +83,15 @@ $(document).ready(function () {
             zwArea: "请选择",
             zwGzzn: "请选择",
             zwGzxz: "请选择",
-            qyLxrdh:  {
-                required: "请填写",
-                email: "请填写正确的11位手机号码"
-            },
             zwLxyx: {
                 required: "请填写",
                 email: "请填写正确电子邮箱"
             },
-            zwXs: "请选择",
             zwZprs: "请填写",
-            zwXs: "请填写",
+            zwXs: {
+                required: "请填写",
+                isNum: "请填写数字"
+            },
             zwNlyq: "请选择",
             zwXlyq: "请选择",
             zwGznx: "请选择",
@@ -116,9 +116,20 @@ $(document).ready(function () {
             $( element ).parent().removeClass( "has-error" );
         }
     });
-
-
 })
+
+function getOneJob() {
+    var jsonObj ={
+        "owid":$("#zwOwid").val(),
+        "yhOwid":getCookie("qyOwid")
+    }
+    ajax("zustjy/bckjBizJob/getOneJob", jsonObj, function (data) {
+        if(data.backCode==0){
+
+
+        }
+    })
+}
 
 $.validator.setDefaults({
     submitHandler: function () {
@@ -143,4 +154,95 @@ function fixJob() {
             });
         }
     })
+}
+
+function zwSubcribeList() {
+    $('#table-job').bootstrapTable('destroy');
+    $('#table-job').bootstrapTable({
+        ajax:function(request) {
+            ajax("zustjy/bckjBizXsgz/zwSubcribeList", {
+                "jobRefOwid":$("#zwOwid").val(),
+                "pageSize":$('#table-job').bootstrapTable('getOptions').pageSize || 10,
+                "pageNo":$('#table-job').bootstrapTable('getOptions').pageNumber || 1
+            }, function (data) {
+                if(data.backCode==0){
+                    request.success({
+                        row : data.bean.records || [],
+                        total: data.bean.totalCount
+                    });
+
+                }
+            })
+        },
+        responseHandler:function(res){
+            $('#table-job').bootstrapTable('load', res.row);
+            return {
+                "total":res.total
+            }
+        },
+        toolbar: '#toolbar', //工具按钮用哪个容器
+        striped: true, //是否显示行间隔色
+        cache: false, //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+        pagination: true, //是否显示分页（*）
+        sortable: true, //是否启用排序
+        sortOrder: "asc", //排序方式
+        sidePagination: "server", //分页方式：client客户端分页，server服务端分页（*）
+        pageNumber: 1, //初始化加载第一页，默认第一页
+        pageSize: 10, //每页的记录行数（*）
+        pageList: [10, 25, 50, 100], //可供选择的每页的行数（*）
+        smartDisplay: false,
+        search: false, //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+        strictSearch: true,
+        showColumns: false, //是否显示所有的列
+        showRefresh: false, //是否显示刷新按钮
+        minimumCountColumns: 2, //最少允许的列数
+        clickToSelect: true, //是否启用点击选中行
+        height: 500, //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+        uniqueId: "owid", //每一行的唯一标识，一般为主键列
+        showToggle: false, //是否显示详细视图和列表视图的切换按钮
+        cardView: false, //是否显示详细视图
+        detailView: false, //是否显示父子表
+        theadClasses: "thead1",
+        queryParamsType:"limit",
+        columns: [{
+            align : 'center',
+            field: 'lxr',
+            title: '联系人',
+        }, {
+            align : 'center',
+            field: 'gzsj',
+            title: '关注时间',
+            formatter:function(value,row,index){
+                var value=""
+                if(row.gzsj){
+                    value=row.gzsj.substring(0,10);
+                }else{
+                    value="-"
+                }
+                return value;
+            }
+        }, {
+            align : 'center',
+            field: 'lxdh',
+            title: '联系电话',
+        }], //列设置
+
+    });
+
+}
+
+function tabs(a) {
+    if(a==0){
+        $("#nav-tabs-job li").removeClass("active")
+        $("#nav-tabs-job li").eq(0).addClass("active")
+        $("#stuList").hide();
+        $("#jobInfo").show();
+    }else if(a==1){
+        zwSubcribeList()
+        $("#nav-tabs-job li").removeClass("active")
+        $("#nav-tabs-job li").eq(1).addClass("active")
+        $("#jobInfo").hide();
+        $("#stuList").show();
+
+    }
 }
