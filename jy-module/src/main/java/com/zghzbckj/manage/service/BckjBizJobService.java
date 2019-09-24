4d77967b3cb2ae669d482afd9a20eee8c09a1d0a
chenchao <51073844@qq.com>
2019-09-24 10:21:13 +0800

    企业相关

--- jy-module/src/main/java/com/zghzbckj/manage/service/BckjBizJobService.java
+++ jy-module/src/main/java/com/zghzbckj/manage/service/BckjBizJobService.java
@@ -406,11 +406,14 @@ public class BckjBizJobService extends CrudService<BckjBizJobDao, BckjBizJob> {
                 job.setQyxx(qyxx);
             }
         }
-        if (!TextUtils.isEmpty(job.getZwYds())) {
-            job.setZwYds(job.getZwYds() + 1);
+//阅读数+1
+        BckjBizJob newJob = get(owid);
+        if (!TextUtils.isEmpty(newJob.getZwYds())) {
+            newJob.setZwYds(newJob.getZwYds() + 1);
         } else {
-            job.setZwYds(1);
+            newJob.setZwYds(1);
         }
+        saveOrUpdate(newJob);
         //查看是否被关注
         if (!TextUtils.isEmpty(mapData.get("yhOwid"))) {
             HashMap<String, Object> sendMap = Maps.newHashMap();
