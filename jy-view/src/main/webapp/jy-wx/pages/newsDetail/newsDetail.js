d83b5e0d54e9fea56996c1b3052bfb93ac369314
zhangqiaojun <897562540@qq.com>
2019-10-15 17:01:37 +0800

    zqj update 超链接文件

--- jy-view/src/main/webapp/jy-wx/pages/newsDetail/newsDetail.js
+++ jy-view/src/main/webapp/jy-wx/pages/newsDetail/newsDetail.js
@@ -34,39 +34,26 @@ Page({
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  wxParseTagATap:function(e){
+    console.log(e.currentTarget.dataset.src);
+    if (e.currentTarget.dataset.src){
+      wx.downloadFile({
+        // 示例 url，并非真实存在
+        url: e.currentTarget.dataset.src,
+        success: function (res) {
+          const filePath = res.tempFilePath
+          wx.openDocument({
+            filePath: filePath,
+            success: function (res) {
+              console.log('打开文档成功')
+            }
+          })
+        },
+        fail:function(){
+          console.log('打开失败')
+        }
+      })
+    }
   }
 })
 var getContent = function (that, owid) {//学校简介
