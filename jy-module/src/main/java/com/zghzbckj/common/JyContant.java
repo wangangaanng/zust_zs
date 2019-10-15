142d74546d94e60ecdb956b7721ccf4a83a842c8
xuyux <zhoubaogang@zghzbckj.com>
2019-10-15 20:34:31 +0800

    xuyux

--- jy-module/src/main/java/com/zghzbckj/common/JyContant.java
+++ jy-module/src/main/java/com/zghzbckj/common/JyContant.java
@@ -67,6 +67,8 @@ public class JyContant {
     public static final String ZW_CITY = "杭州市";
     public static final String ZW_AREA = "西湖区";
     public static final String ZW_DD = "浙江科技学院";
-
-
+    //中国地图省份
+    public static final String[] provinceList = {"北京", "天津", "上海", "重庆", "河北", "河南", "云南", "辽宁", "黑龙江", "湖南", "安徽", "山东",
+        "新疆", "江苏", "浙江", "江西", "湖北", "广西", "甘肃", "山西", "内蒙古", "陕西", "吉林", "福建", "贵州", "广东", "青海", "西藏",
+        "四川", "宁夏", "海南", "台湾", "香港", "澳门"};
 }
