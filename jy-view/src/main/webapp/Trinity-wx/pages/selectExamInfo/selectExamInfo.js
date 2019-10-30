72476b35bbadf7d4a5aab4896fb1431bfd876fbb
root <1327165584@qq.com>
2019-10-30 15:37:48 +0800

    2019.10.30 于铭杰 三位一体

--- jy-view/src/main/webapp/Trinity-wx/pages/selectExamInfo/selectExamInfo.js
+++ jy-view/src/main/webapp/Trinity-wx/pages/selectExamInfo/selectExamInfo.js
@@ -2,150 +2,267 @@
 var common = require('../../libs/common/common.js')
 const app = getApp()
 var url = app.globalData.ApiUrl;
-var yhRefOwid = app.globalData.yhRefOwid;
+var yhRefOwid = wx.getStorageSync('yhRefOwid');
+var jsfj = []; //附件owid
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    showPop:false,
-    subjectList: ["科目一","科目二","科目三","科目四"],
-    selectSubject:[],
-    result: ['a', 'b']
+    showPop: false, //显示选择
+    subjectList: [], //显示选项
+    selectSubject: [], //显示选项
+    files: [], //证书照片
+    zx: [], //专项信息
+    zxindex: null,
+    zxlist: [], //专项信息
+    zxtype: [],
+    xk: [], //选考信息
+    xkindex: [{
+      value: null,
+      index: null,
+    }, {
+      value: null,
+      index: null,
+    }, {
+      value: null,
+      index: null,
+    }],
+    xklist: [], //选考信息
+    wy: [], //外语
+    wyindex: null,
+    wylist: [], //外语
+    wycj: '', //外语成绩
+    jssm: '', //竞赛说明
+    qtqk: '', //其他情况
+    tcah: '', //特长爱好
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.getByType()
+  onLoad: function(options) {
+    this.getByType('10020')
+    this.getByType('10024')
     this.getXkkm()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  Image(){
+  chooseImage() {
+    var that = this;
     wx.chooseImage({
       count: 1,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success(res) {
-        // tempFilePath可以作为img标签的src属性显示图片
-        const tempFilePaths = res.tempFilePaths
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function(res) {
+        console.log(res)
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+        common.uploadFile(res.tempFilePaths, 2, function(res) {
+          let data = JSON.parse(res.data)
+          if (data.backCode == 0) {
+            jsfj.push(data.bean.owid)
+          } else {
+            common.toast('上传失败', 'none', 2000);
+          }
+        });
       }
     })
   },
-  //确定选择
-  confirmSubject(event){
-    const { value, index } = event.detail;
-    this.data.selectSubject = value;
+  previewImage: function(e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
+  xkcj(e) {
+    let value = e.detail.value
+    let index = e.target.dataset.index
+    let xkindex = this.data.xkindex
+    xkindex[index].value = value
     this.setData({
-      showPop: true,
-      ["selectSubject[1]"]: this.data.selectSubject
+      xkindex
+    })
+  },
+  wycj(e) {
+    let value = e.detail.value
+    this.setData({
+      wycj: value
+    })
+  },
+  jssm(e) {
+    this.setData({
+      jssm: e.detail.value
+    })
+  },
+  qtqk(e) {
+    this.setData({
+      qtqk: e.detail.value
+    })
+  },
+  tcah(e) {
+    this.setData({
+      tcah: e.detail.value
+    })
+  },
+
+  //确定选择
+  confirmSubject(e) {
+    console.log(e)
+    if (this.data.xkeq != null || this.data.xkeq != undefined) {
+      let xkindex = this.data.xkindex
+      xkindex[this.data.xkeq].index = e.detail.index
+      this.setData({
+        xkindex,
+        showPop: false
+      })
+    } else {
+      let wyindex = e.detail.index
+      this.setData({
+        wyindex,
+        showPop: false
+      })
+    }
+  },
+  onChange(event) {
+    this.setData({
+      zxtype: event.detail
     });
   },
   //隐藏弹出
-  cancelPop(){
+  cancelPop() {
     this.setData({
       showPop: false
     });
   },
   //点击弹出
-  selectSubject(){
+  selectSubject(e) {
+    console.log(e)
+    let type = e.currentTarget.dataset.type;
+    let index;
+    let subjectList = []
+    if (type == "0") {
+      index = e.currentTarget.dataset.index;
+      subjectList = this.data.xklist
+    }
+    if (type == "1") {
+      subjectList = this.data.wylist
+    }
     this.setData({
-      showPop: true
+      showPop: true,
+      subjectList,
+      xkeq: index
     });
   },
-  onChange(event) {
-    this.setData({
-      result: event.detail
-    });
-  },
-  preStep:function(){
+  preStep: function() {
     wx.navigateTo({
       url: '../examGrade/examGrade',
     })
   },
-  finish:function(){
+  finish: function() {
     wx.navigateTo({
       url: '../Process/Process',
     })
   },
-  //获取选课列表
-  getXkkm: function (e) {
+  //获取选考列表
+  getXkkm: function(e) {
     let that = this;
     let data = {
-      dicType:'10022',
-      dicVal5:'1'
+      dicType: '10022',
+      dicVal5: '1'
     }
-    common.ajax('zustcommon/common/getXkkm', data, function (res) {
+    common.ajax('zustcommon/common/getXkkm', data, function(res) {
       if (res.data.backCode == 0) {
+        let xklist = [];
+        for (let i in res.data.bean) {
+          xklist.push(res.data.bean[i].dicval2)
+        }
         that.setData({
-          xk: res.data.bean
+          xk: res.data.bean,
+          xklist: xklist
         })
       } else {
         common.toast(res.data.errorMess, 'none', 2000)
       }
     });
   },
-  //获取外语语种
-  getByType: function (e) {
+  //获取字典表信息
+  getByType: function(e) {
     let that = this;
     let data = {
-      dicType: '10020',
+      dicType: e,
     }
-    common.ajax('zustcommon/common/getByType', data, function (res) {
+    common.ajax('zustcommon/common/getByType', data, function(res) {
       if (res.data.backCode == 0) {
-        that.setData({
-          wy:res.data.bean
-        })
+        if (e == '10020') {
+          let wylist = [];
+          for (let i in res.data.bean) {
+            wylist.push(res.data.bean[i].dicVal2)
+          }
+          that.setData({
+            wy: res.data.bean,
+            wylist: wylist
+          })
+        } else if (e == '10024') {
+          let zxlist = [];
+          for (let i in res.data.bean) {
+            zxlist.push(res.data.bean[i].dicVal2)
+          }
+          that.setData({
+            zx: res.data.bean,
+            zxlist: zxlist
+          })
+        }
       } else {
         common.toast(res.data.errorMess, 'none', 2000)
       }
