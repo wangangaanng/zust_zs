f8c7b97ac3fe0ab86aeda5c1a97125652249aaba
xiayuwei <2515421994@qq.com>
2019-10-31 11:57:28 +0800

    发送邮箱

--- jy-view/src/main/webapp/Trinity-wx/pages/sendMail/sendMail.js
+++ jy-view/src/main/webapp/Trinity-wx/pages/sendMail/sendMail.js
@@ -2,18 +2,21 @@
 var common = require('../../libs/common/common.js');
 var utils = require('../../utils/util.js');
 import WxValidate from '../../utils/WxValidate';
+import Dialog from '../../miniprogram_npm/vant-weapp/dialog/dialog';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    mailAddress:"",
+    mailAddress: "",
     imgPath: common.imgPath,
-    filePath:''
+    filePath: ''
   },
+  
   //发送邮箱
-  sendMailForm(e){
+  sendMailForm(e) {
+    let that = this;
     const params = e.detail.value;
     //传入表单数据，调用验证方法
     if (!this.WxValidate.checkForm(params)) {
@@ -21,7 +24,34 @@ Page({
       common.toast(error.msg, 'none', 2000)
       return false
     }
+    Dialog.confirm({
+      title: '确认邮箱地址',
+      message: '报名表将发送到：'+this.data.mailAddress
+    }).then(() => {
+      that.setData({
+        "mailAddress": params.email
+      })
+      wx.setStorageSync(key, data)
+      // 确认发送邮箱
+      this.ajaxSend();
+    }).catch(() => {// on cancel
+    });
+
   },
+
+  ajaxSend: function () {
+    var data = {
+      "applyOwid": wx.getStorageSync("sqbOwid"),//申请表owid
+    }
+    common.ajax('zustswyt/bckjBizBm/sendView', data, function (res) {
+      if (res.data.backCode == 0) {
+        common.toast('报名表已成功发送到'+this.data.mailAddress+',请注意查收', 'none', 2000)
+      } else {
+        common.toast(res.data.errorMess, 'none', 2000)
+      }
+    });
+  },
+
   //预览pdf:报名表
   openFile: function (e) {
     console.log(e.currentTarget.dataset.src);
@@ -44,10 +74,12 @@ Page({
       })
     }
   },
-  onLoad:function(options){
-    var eamil = wx.getStorageSync("eamil");
+
+  onLoad: function (options) {
+    this.initValidate();
+    var email = wx.getStorageSync("email");
     this.setData({
-      mailAddress: eamil,
+      mailAddress: email,
     });
     this.getFileUrl();
   },
@@ -58,9 +90,9 @@ Page({
 
   },
   //获取pdf文件路径
-  getFileUrl:function(){
+  getFileUrl: function () {
     var data = {
-      "applyOwid": wx.getStorageSync("yhRefOwid")
+      "applyOwid": wx.getStorageSync("sqbOwid"),//申请表owid
     }
     common.ajax('zustswyt/bckjBizBm/getApply', data, function (res) {
       if (res.data.backCode == 0) {
@@ -71,5 +103,22 @@ Page({
         common.toast(res.data.errorMess, 'none', 2000)
       }
     });
+  },
+  initValidate() {
+    // 验证字段的规则
+    const rules = {
+      email: {
+        required: true,
+        email: true,
+      }
+    }
+
+    // 验证字段的提示信息，若不传则调用默认的信息
+    const messages = {
+      email: {
+        required: '请输入邮箱地址'
+      }
+    }
+    this.WxValidate = new WxValidate(rules, messages)
   }
 })
