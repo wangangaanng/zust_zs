c0f86f876a9f32b96c664413fc446b684c81b3e2
xiayuwei <2515421994@qq.com>
2019-10-31 17:14:39 +0800

    在线支付+签名图片上传+设置toast默认时间

--- jy-view/src/main/webapp/Trinity-wx/pages/payOline/payOline.js
+++ jy-view/src/main/webapp/Trinity-wx/pages/payOline/payOline.js
@@ -1,18 +1,101 @@
 // pages/payOline/payOline.js
+var common = require('../../libs/common/common.js');
+import WxValidate from '../../utils/WxValidate';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    jfInfo: '缴费证明图片上传',
+    class1: '',
+    proveImg: '',
+    upProveImg: ''
+  },
 
+  //选择图片
+  choseImageProve(event) {
+    var that = this;
+    var type = event.currentTarget.dataset.type;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        const tempFilePaths = res.tempFilePaths;
+
+        //上传图片
+        common.uploadFile(tempFilePaths, 1, function (res) {
+          let data = JSON.parse(res.data)
+          if (data.backCode == 0) {
+            let imgUrl = data.bean.filePath;
+            that.setData({
+              upProveImg: imgUrl,
+              jfInfo: '缴费证明图片上传成功',
+              class1: 'green'
+            })
+          } else {
+            that.setData({
+              jfInfo: '缴费证明图片上传失败，请重传',
+              class1: 'red'
+            })
+            common.toast('上传失败', 'none', 2000);
+          }
+        });
+
+        //图片临时显示
+        that.setData({
+          proveImg: tempFilePaths[0]
+        });
+
+      }
+    })
+  },
+
+  //上传缴费证明
+  upPayImg: function () {
+    var that = this;
+    if (this.data.upProveImg == "") {
+      common.toast('请先上传缴费证明图片', 'none')
+      return;
+    }
+    var data = {
+      "applyOwid": wx.getStorageSync("sqbOwid"),
+      "jfpzZp": that.data.upProveImg
+    }
+    common.ajax('zustswyt/bckjBizBm/submitJft', data, function (res) {
+      if (res.data.backCode == 0) {
+        Dialog.alert({
+          message: '缴费证明图片已成功提交，请等待审核！'
+        }).then(() => {
+          wx.navigateTo({
+            url: '../Process/Process',
+          })
+        });
+      } else {
+        common.toast(res.data.errorMess, 'none')
+      }
+    });
+  },
+
+  //点击图片放大
+  previewImage: function (event) {
+    let src = event.currentTarget.dataset.src;//获取data-src
+    let imgArr = [src]
+    wx.previewImage({
+      current: src,     //当前图片地址
+      urls: imgArr,               //所有要预览的图片的地址集合 数组形式
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    getPayUrl();//获取缴费初始化地址
   },
 
   /**
@@ -30,37 +113,23 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
 
   }
 })
+
+//初始化获取缴费地址
+function getPayUrl(){
+  var data = {
+    "dicType": '10025',//字典表缴费说明
+  }
+  common.ajax('zustcommon/common/getByType', data, function (res) {
+    if (res.data.backCode == 0) {
+      
+    } else {
+      common.toast(res.data.errorMess, 'none', 2000)
+    }
+  });
+}
