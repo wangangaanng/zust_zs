c88b86e90c12a2c07765525599c129d60dd66af1
xiayuwei <2515421994@qq.com>
2019-11-06 20:29:54 +0800

    性别增加*  个人中心如果没有

--- jy-view/src/main/webapp/Trinity-wx/pages/grzx/grzx.js
+++ jy-view/src/main/webapp/Trinity-wx/pages/grzx/grzx.js
@@ -39,14 +39,17 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    if (!wx.getStorageSync('yhRefOwid')) {
+      wx.navigateTo({
+        url: '../login/login'
+      })
+    } 
   },
 
   /**
