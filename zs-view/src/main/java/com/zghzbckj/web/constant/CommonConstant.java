11c7d3779c7ca62b6f3ce390fdd4dc7e08563cfb
xiayuwei <2515421994@qq.com>
2019-11-14 13:47:23 +0800

    还原配置文件

--- zs-view/src/main/java/com/zghzbckj/web/constant/CommonConstant.java
+++ zs-view/src/main/java/com/zghzbckj/web/constant/CommonConstant.java
@@ -126,16 +126,14 @@ public final class CommonConstant {
     public static String PDF_FOOTER2_KEY = "pdfFooter2";
 
     public final static String EMPTY_STR ="";
-    public final static String picPath = "D:\\Java\\apache-tomcat-8.0.26\\webapps\\projectManageFiles";
-    public final static String excelPath = "/mnt/files/zjcFiles/excel";
+
     public final static String PIC_PNG = "PNG";
     public final static String PIC_JPG = "JPG";
     public final static String PIC_JPEG = "JPEG";
     public final static String SPILT_POINT = ".";
+    public final static String picPath = "/mnt/files/zjcFiles/pic";
     public final static String ID_URL = "https://aip.baidubce.com/rest/2.0/ocr/v1/idcard?access_token=";
     public final static String LICENSE_URL = "https://aip.baidubce.com/rest/2.0/ocr/v1/business_license?access_token=";
-    public static String APP_ID = "17232634";
     public static String API_KEY = "dMST2glOiL4Z1RCx6I42AG0N";
     public static String SECRET_KEY = "QQ2PADGVib0vzlz0GZN9EpSRFtKtZla4";
-
 }
